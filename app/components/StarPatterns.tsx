e5d610a9200f91249b97fda498532e7016e8776d
lilpacy <revivedtomorrow@gmail.com>
2025-06-13 23:48:15 +0900

    Revert "next.config.tsにエクスポート設定を追加し、StarPatternsコンポーネントで動的な画像パスを取得するカスタムフックを実装しました。これにより、GitHub Pagesでの画像表示が最適化されました。"
    This reverts commit 7e72214a866faea2641748fdfc8835b3d07430c0.

--- app/components/StarPatterns.tsx
+++ app/components/StarPatterns.tsx
@@ -1,29 +1,11 @@
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
-
-// パスを動的に取得するカスタムフック
-function useAssetPath() {
-  const [basePath, setBasePath] = useState('');
-  
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      // GitHub Pages の場合は /under-rebranding を追加
-      const isGitHubPages = window.location.hostname === 'animechain-ai.github.io';
-      setBasePath(isGitHubPages ? '/under-rebranding' : '');
-    }
-  }, []);
-  
-  return basePath;
-}
 
 // 左上 - デスクトップ用
 export function TopLeftStarsDesktop() {
-  const basePath = useAssetPath();
-  
   return (
     <div className="absolute left-1/2 top-6 -translate-x-[400px]">
       <Image
-        src={`${basePath}/desktop/top-left.png`}
+        src="/desktop/top-left.png"
         alt="Stars decoration"
         width={160}
         height={99}
@@ -35,12 +17,10 @@ export function TopLeftStarsDesktop() {
 
 // 左上 - モバイル用
 export function TopLeftStarsMobile() {
-  const basePath = useAssetPath();
-  
   return (
     <div className="absolute left-4 top-4">
       <Image
-        src={`${basePath}/mobile/top-left.png`}
+        src="/mobile/top-left.png"
         alt="Stars decoration"
         width={100}
         height={97}
@@ -52,12 +32,10 @@ export function TopLeftStarsMobile() {
 
 // 右下 - デスクトップ用
 export function BottomRightStarsDesktop() {
-  const basePath = useAssetPath();
-  
   return (
     <div className="absolute left-1/2 bottom-6 translate-x-[300px]">
       <Image
-        src={`${basePath}/desktop/bottom-right.png`}
+        src="/desktop/bottom-right.png"
         alt="Stars decoration"
         width={111}
         height={99}
@@ -69,12 +47,10 @@ export function BottomRightStarsDesktop() {
 
 // 右下 - モバイル用
 export function BottomRightStarsMobile() {
-  const basePath = useAssetPath();
-  
   return (
     <div className="absolute right-4 bottom-4">
       <Image
-        src={`${basePath}/mobile/bottom-right.png`}
+        src="/mobile/bottom-right.png"
         alt="Stars decoration"
         width={95}
         height={93}
